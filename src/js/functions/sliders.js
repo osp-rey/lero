3d1678058c934b28bb4f3f01479e0793b40473ff
pauchok-sk <pauchoooooook@gmail.com>
2026-09-06 16:31:13 +0300

    s-portfolio

--- src/js/functions/sliders.js
+++ src/js/functions/sliders.js
@@ -1,16 +1,44 @@
 export default function sliders() {
-  const portfolioSlider = document.querySelector(".s-portfolio__slider");
+  const portfolioSliders = document.querySelectorAll(".s-portfolio__slider");
 
-  if (portfolioSlider) {
-    const swiper = new Swiper(portfolioSlider, {
-      speed: 900,
-      slidesPerView: 2,
-      spaceBetween: 20,
-      navigation: {
-        prevEl: ".s-portfolio__nav-wrap .slider-arrow._prev",
-        nextEl: ".s-portfolio__nav-wrap .slider-arrow._next",
-      },
-    });
+  if (portfolioSliders.length) {
+    portfolioSliders.forEach(slider => {
+      const parentTab =  slider.closest("[data-tab]");
+      const idTab = parentTab.dataset.tab;
+
+      const swiper = new Swiper(slider, {
+        speed: 900,
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+          prevEl: `.s-portfolio__nav-wrap [data-tab="${idTab}"] .slider-arrow._prev`,
+          nextEl: `.s-portfolio__nav-wrap [data-tab="${idTab}"] .slider-arrow._next`,
+        },
+        breakpoints: {
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        },
+        scrollbar: {
+          el: parentTab.querySelector(".slider-scrollbar"),
+          draggable: true,
+        },
+        on: {
+          touchStart: function (swiper, event) {
+            const isProductSlider = event.target.closest(
+              ".card-portfolio__slider",
+            );
+            if (isProductSlider) {
+              swiper.allowTouchMove = false;
+            }
+          },
+          touchEnd: function (swiper) {
+            swiper.allowTouchMove = true;
+          },
+        },
+      });
+    })
   }
 
   const cardPortfolioSliders = document.querySelectorAll(
@@ -24,6 +52,10 @@ export default function sliders() {
         navigation: {
           prevEl: slider.querySelector(".slider-arrow._prev"),
           nextEl: slider.querySelector(".slider-arrow._next"),
+        },
+        pagination: {
+          el: slider.querySelector(".slider-pagination"),
+          clickable: true,
         },
       });
     });
